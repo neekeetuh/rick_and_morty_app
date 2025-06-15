f9ecd2396b37c522dea09ad6906247b73ea1c13b
neekeetuh <nikitakhimchenko@gmail.com>
2025-06-15 15:29:10 +0600

    feat: implemented custom animation for adding characters to favorites

--- lib/src/features/characters/presentation/widgets/character_card.dart
+++ lib/src/features/characters/presentation/widgets/character_card.dart
@@ -7,10 +7,35 @@ import 'package:rick_and_morty_app/src/common/consts/text_styles_consts.dart';
 import 'package:rick_and_morty_app/src/features/characters/domain/models/character.dart';
 import 'package:rick_and_morty_app/src/features/characters/presentation/bloc/bloc/characters_bloc.dart';
 
-class CharacterCard extends StatelessWidget {
+class CharacterCard extends StatefulWidget {
   final Character character;
 
   const CharacterCard({super.key, required this.character});
+
+  @override
+  State<CharacterCard> createState() => _CharacterCardState();
+}
+
+class _CharacterCardState extends State<CharacterCard>
+    with SingleTickerProviderStateMixin<CharacterCard> {
+  late AnimationController _controller;
+
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this, duration: Durations.short4);
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.5).animate(
+        CurvedAnimation(parent: _controller, curve: Curves.easeOutSine));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,122 +45,148 @@ class CharacterCard extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: CachedNetworkImage(
-                imageUrl: character.imageUrl,
-                width: imageWidth,
-                height: imageWidth,
-                fit: BoxFit.cover,
-                placeholder: (context, _) => ColoredBox(
-                  color: Colors.grey[300]!,
-                  child: const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                ),
-                errorWidget: (context, url, error) => SizedBox(
-                  width: imageWidth,
-                  height: imageWidth,
-                  child: ColoredBox(
-                    color: Colors.grey[300]!,
-                    child: Icon(
-                      Icons.broken_image,
-                      color: Colors.grey,
-                      size: imageWidth * 0.5,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: CachedNetworkImage(
+                    imageUrl: widget.character.imageUrl,
+                    width: imageWidth,
+                    height: imageWidth,
+                    fit: BoxFit.cover,
+                    placeholder: (context, _) => ColoredBox(
+                      color: Colors.grey[300]!,
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => SizedBox(
+                      width: imageWidth,
+                      height: imageWidth,
+                      child: ColoredBox(
+                        color: Colors.grey[300]!,
+                        child: Icon(
+                          Icons.broken_image,
+                          color: Colors.grey,
+                          size: imageWidth * 0.5,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(width: 12.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                const SizedBox(width: 12.0),
+                Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Expanded(
-                        child: Text(
-                          character.name,
-                          style: TextStylesConsts.header2,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              widget.character.name,
+                              style: TextStylesConsts.header2,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 30,
-                        width: 30,
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          iconSize: 28,
-                          icon: Icon(
-                            character.isFavorite
-                                ? Icons.star
-                                : Icons.star_border,
-                            color: character.isFavorite
+                      const SizedBox(height: 8.0),
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 10,
+                            width: 10,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                color: widget.character.status.statusColor,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 6.0),
+                          Expanded(
+                            child: Text(
+                              "${StringConsts.characterCardStatus}: ${widget.character.status.statusText}",
+                              style: TextStylesConsts.caption2
+                                  .copyWith(color: Colors.grey[700]),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text(
+                        "${StringConsts.characterCardSpecies}: ${widget.character.species}",
+                        style: TextStylesConsts.caption2
+                            .copyWith(color: Colors.grey[700]),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text(
+                        "${StringConsts.characterCardLoaction}: ${widget.character.lastKnownLocation}",
+                        style: TextStylesConsts.caption2
+                            .copyWith(color: Colors.grey[700]),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: AnimatedBuilder(
+                  animation: _scaleAnimation,
+                  builder: (context, child) {
+                    return SizedBox(
+                      height: 30 * _scaleAnimation.value,
+                      width: 30 * _scaleAnimation.value,
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        iconSize: 28 * _scaleAnimation.value,
+                        icon: TweenAnimationBuilder(
+                          tween: ColorTween(
+                            end: widget.character.isFavorite
                                 ? ColorConsts.favoriteIconChosen
                                 : ColorConsts.favoriteIconHollow,
+                            begin: widget.character.isFavorite
+                                ? ColorConsts.favoriteIconHollow
+                                : ColorConsts.favoriteIconChosen,
                           ),
-                          onPressed: () {
-                            context
-                                .read<CharactersBloc>()
-                                .add(ToggleFavoriteEvent(character: character));
+                          duration: Durations.long1,
+                          builder: (context, value, child) {
+                            return Icon(
+                              widget.character.isFavorite
+                                  ? Icons.star
+                                  : Icons.star_border,
+                              color: value,
+                            );
                           },
                         ),
+                        onPressed: _onToggleFavoritePressed,
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 10,
-                        width: 10,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: character.status.statusColor,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 6.0),
-                      Expanded(
-                        child: Text(
-                          "${StringConsts.characterCardStatus}: ${character.status.statusText}",
-                          style: TextStylesConsts.caption2
-                              .copyWith(color: Colors.grey[700]),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 4.0),
-                  Text(
-                    "${StringConsts.characterCardSpecies}: ${character.species}",
-                    style: TextStylesConsts.caption2
-                        .copyWith(color: Colors.grey[700]),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const SizedBox(height: 4.0),
-                  Text(
-                    "${StringConsts.characterCardLoaction}: ${character.lastKnownLocation}",
-                    style: TextStylesConsts.caption2
-                        .copyWith(color: Colors.grey[700]),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                  ),
-                ],
-              ),
-            ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
     );
+  }
+
+  _onToggleFavoritePressed() async {
+    context
+        .read<CharactersBloc>()
+        .add(ToggleFavoriteEvent(character: widget.character));
+    await _controller.forward();
+    await _controller.reverse();
   }
 }
