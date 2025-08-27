f45114bbf2a18ae1327677ab3991eda9cad9f0cc
neekeetuh <nikitakhimchenko@gmail.com>
2025-08-27 15:41:35 +0600

    ci: fixed the workflow and bash script for running tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+# Захватываем ID эмулятора из аргумента
+EMULATOR_ID=$2
+if [ -z "$EMULATOR_ID" ]; then
+    # Если аргумент не был передан, находим ID эмулятора автоматически
+    echo "ID эмулятора не предоставлен, ищем автоматически..."
+    EMULATOR_ID=$(adb devices | grep emulator | cut -f1)
+fi
+
+# Проверяем, найден ли эмулятор
+if [ -z "$EMULATOR_ID" ]; then
+    echo "Ошибка: Эмулятор не найден. Выход."
+    exit 1
+fi
+
 echo "--- Начинаем подготовку проекта ---"
 
 flutter clean
@@ -17,16 +31,8 @@ echo "--- Запускаем виджет-тесты ---"
 flutter test test/widget/characters_list_screen_widget_test.dart
 
 echo "--- Запускаем интеграционные тесты ---"
-EMULATOR_ID=$(adb devices | grep emulator | cut -f1)
 
-# Check if an emulator was found.
-if [ -z "$EMULATOR_ID" ]; then
-  echo "Error: No emulator found. Exiting."
-  exit 1
-fi
-
-echo "Found emulator: $EMULATOR_ID"
-
+echo "Используемый эмулятор: $EMULATOR_ID"
 # The command to run integration tests on the detected emulator.
 flutter test integration_test/integration_test.dart -d "$EMULATOR_ID"
 
