e3b8898eb4e87726fc7a17e40d541922ca489edf
neekeetuh <nikitakhimchenko@gmail.com>
2025-08-27 15:54:48 +0600

    ci: fixed the run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -3,18 +3,7 @@
 set -e
 
 # Захватываем ID эмулятора из аргумента
-EMULATOR_ID=$2
-if [ -z "$EMULATOR_ID" ]; then
-    # Если аргумент не был передан, находим ID эмулятора автоматически
-    echo "ID эмулятора не предоставлен, ищем автоматически..."
-    EMULATOR_ID=$(adb devices | grep emulator | cut -f1)
-fi
-
-# Проверяем, найден ли эмулятор
-if [ -z "$EMULATOR_ID" ]; then
-    echo "Ошибка: Эмулятор не найден. Выход."
-    exit 1
-fi
+EMULATOR_ID=$1
 
 echo "--- Начинаем подготовку проекта ---"
 
@@ -32,8 +21,17 @@ flutter test test/widget/characters_list_screen_widget_test.dart
 
 echo "--- Запускаем интеграционные тесты ---"
 
-echo "Используемый эмулятор: $EMULATOR_ID"
+# Создаем переменную, которая будет содержать флаг -d, если ID эмулятора передан.
+# Если ID не передан, переменная будет пустой, и флаг не будет использоваться.
+DEVICE_FLAG=""
+if [ -n "$EMULATOR_ID" ]; then
+    echo "Используемый эмулятор: $EMULATOR_ID"
+    DEVICE_FLAG="-d $EMULATOR_ID"
+else
+    echo "ID эмулятора не указан, flutter test будет искать его автоматически."
+fi
+
 # The command to run integration tests on the detected emulator.
-flutter test integration_test/integration_test.dart -d "$EMULATOR_ID"
+flutter test integration_test/integration_test.dart $DEVICE_FLAG"
 
 echo "--- Все тесты успешно выполнены! ---"
