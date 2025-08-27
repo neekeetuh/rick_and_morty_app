d64b5a4cab65ee591d51a441fdc7173dbdcbc415
neekeetuh <nikitakhimchenko@gmail.com>
2025-08-28 01:44:10 +0600

    ci: simplified the scripts

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Захватываем ID эмулятора из аргумента
-EMULATOR_ID=$1
-
 echo "--- Начинаем подготовку проекта ---"
 
 flutter clean
@@ -21,17 +18,6 @@ flutter test test/widget/characters_list_screen_widget_test.dart
 
 echo "--- Запускаем интеграционные тесты ---"
 
-# Создаем переменную, которая будет содержать флаг -d, если ID эмулятора передан.
-# Если ID не передан, переменная будет пустой, и флаг не будет использоваться.
-DEVICE_FLAG=""
-if [ -n "$EMULATOR_ID" ]; then
-    echo "Используемый эмулятор: $EMULATOR_ID"
-    DEVICE_FLAG="-d $EMULATOR_ID"
-else
-    echo "ID эмулятора не указан, flutter test будет искать его автоматически."
-fi
-
-# The command to run integration tests on the detected emulator.
-flutter test integration_test/integration_test.dart "$DEVICE_FLAG"
+flutter test integration_test/integration_test.dart
 
 echo "--- Все тесты успешно выполнены! ---"
